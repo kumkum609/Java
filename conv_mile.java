ad33ac642b74f951c02674906561a31b2d77a7e6
kumkum Gautam <kumkumg609@gmail.com>
2024-11-15 15:12:57 +0530

    conv_mile

---
+++ conv_mile.java
@@ -0,0 +1,12 @@
+public class conv_mile {
+
+    public static void main(string[]args){
+        //convert kilometre into miles
+        System.out.println("Enter the value in kilometre:");
+        Scanner.sc=new Scanner(System.in);
+        float a=sc.next Float;
+        float convert =a/1.60f;
+        System.out.println("Miles"+convert);
+    }
+}
+
