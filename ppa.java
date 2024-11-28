6a1e6da8310fdc22c72ae479b0a3781afc564aeb
kumkum Gautam <kumkumg609@gmail.com>
2024-11-28 18:17:07 +0530

    enter,java

---
+++ ppa.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ppa {
+
+
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the value of a:");
+            int a= sc.nextInt();
+
+            System.out.println(a>=8);
+
+
+        }
+    }
+
+
+
