d0654b78fbc526eae4ff8686d9c1b6c88f6c728b
kumkum Gautam <kumkumg609@gmail.com>
2025-02-01 16:10:35 +0530

    str.java

---
+++ str_qs.java
@@ -0,0 +1,13 @@
+public class str_qs {
+    public static void main(String[] args) {
+//problem 1 convert a string to lowercase
+        string name = "KuMkum";
+        name = name.ToLowerCase();
+        System.out.println(name);
+    //problem 2 replace spaces with underscore
+      //  stirng text="To my friends";
+        //text=text.replace(" ","_");
+       // System.out.println(text);
+
+    }
+}
