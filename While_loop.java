fd9963c99797bb4ac6a7aa22c20c98fb4d23a322
kumkum Gautam <kumkumg609@gmail.com>
2025-02-09 14:26:57 +0530

    While_loop

---
+++ While_loop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class While_loop {
+    public static void main(String[] args) {
+        //Print number by while loop
+        int a=0;
+        while(a<=5)
+            System.out.println(i);
+        a++;
+
+        //Print number-->User define
+        Scanner sc=new Scanner(System.in);
+        int i,n;
+        System.out.println("Enter the value of i:");
+        i=sc.nextInt();
+        System.out.println("Enter the value of n:");
+        n=sc.nextInt();
+        while(i<=n)
+            System.out.println(i);
+        i++;
+        
+    }
+
+}
