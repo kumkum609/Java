bc779f922e6c2ebe1501b46e01dfdf9f9c14bc13
kumkum Gautam <kumkumg609@gmail.com>
2025-02-11 15:27:58 +0530

    forloop

---
+++ for_loop.java
@@ -0,0 +1,16 @@
+public class for_loop {
+    public static void main(String[] args) {
+
+        /*Print numbers to 10 by for loop
+
+        for(int i=0;i<=10;i++){
+         System.out.println(i);
+        }*/
+
+        //Print odd number
+
+        for(int n=5;n<5;n++){
+            System.out.println(2*n+1);
+        }
+    }
+}
