09167bb63d92a96506860397c1258256b7ddcf88
kumkum Gautam <kumkumg609@gmail.com>
2025-02-12 14:04:46 +0530

    break

---
+++ Break.java
@@ -0,0 +1,17 @@
+public class Break {
+    public static void main(String[] args) {
+
+        //break
+
+        for(int i=0;i<=5;i++){
+            System.out.println(i);
+            if(i==2){
+
+                System.out.println("End the loop");
+                break;
+            }
+        }
+
+
+    }
+}
