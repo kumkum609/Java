24c7ddbd203a1e103299645f371e97a51271cf82
kumkum Gautam <kumkumg609@gmail.com>
2025-02-21 12:26:57 +0530

    array

---
+++ Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args) {
+
+        /*Displaying an array
+
+        int[]marks={98,48,90,34,56};
+        System.out.println(marks.length); */
+
+        //Printing using for loop
+
+        int[]marks={54,78,90,76,29};
+        System.out.println("Print the marks:");
+        for(int i=0;i<marks.length;i++)
+            System.out.println(marks[i]);
+    }
+}
