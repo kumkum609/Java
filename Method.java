d2f1d25315ebd67611cc64e93997168e5f25499b
kumkum Gautam <kumkumg609@gmail.com>
2025-03-03 13:47:54 +0530

    .

---
+++ Method.java
@@ -0,0 +1,46 @@
+public class Method {
+    //Methods in java
+
+    static int logic(int x,inty){
+        int z;
+        if(x>y){
+            z=x+y;
+        }
+        else{
+            z=(x+y)*6;
+        }
+        return z;
+    }
+    public static void main(String[] args) {
+        int a=5;
+        int b=9;
+        int c;
+        if(a>b){
+            c=a+b;
+        }
+        else{
+            c=(a+b)*6;
+        }
+        int a=5;
+        int b=9;
+        int c;
+        if(a>b){
+            c=a+b;
+        }
+        else{
+            c=(a+b)*6;
+        }
+        int a1=5;
+        int b1=9;
+        int c1;
+        if(a1>b1){
+            c1=a1+b1;
+        }
+        else{
+            c1=(a1+b1)*6;
+        }
+        System.out.println(c);
+        System.out.println(c1);
+
+    }
+}
