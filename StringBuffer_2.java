2856207f546b8cadd6f49d646ad8e6095e168050
kumkum Gautam <kumkumg609@gmail.com>
2025-07-12 13:45:21 +0530

    Add files via upload

---
+++ StringBuffer_2.java
@@ -0,0 +1,7 @@
+public class StringBuffer_2 {
+    public static void main(String[]args){
+        StringBuffer sb = new StringBuffer("Intelligt Person");
+        sb.insert(6, "en");
+    }
+    
+}
