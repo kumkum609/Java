e14bb2e8529a40603f5769cbf01eefeba517800b
kumkum Gautam <kumkumg609@gmail.com>
2025-07-13 16:46:57 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,11 @@
+public class Concat {
+    public static void main(String[] args) {
+        // Concatenation==> it merge the words
+  String s1="Uni",s2="ver",s3="sity";
+        String result;
+   result=s1.concat(s2);
+        result+=s3;
+ System.out.println("After concatenation string = " +result);
+
+    }
+}
