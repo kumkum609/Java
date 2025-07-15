9cae53156669754a6306afe4786f14ff0e206045
kumkum Gautam <kumkumg609@gmail.com>
2025-07-15 11:11:33 +0530

    Add files via upload

---
+++ Case.java
@@ -0,0 +1,16 @@
+public class Case {
+    public static void main(String[] args) {
+             //Convert the string to lowercase
+
+        String str = "Hello World!";
+        String sr = str.toLowerCase();
+             // Print the convert string 
+        System.out.println("Lower case string =" +sr);
+
+            // Convet the string to uppercase
+        String a = "Hello World!";
+        String b = a.toUpperCase();
+        System.out.println("upper case string = " +b);
+    }
+    
+}
