cc25f12f804278d5ad47aceda8265173e6f96122
kumkum Gautam <kumkumg609@gmail.com>
2025-07-29 11:46:47 +0530

    Add files via upload

---
+++ MyArrayListDemo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MyArrayListDemo {
+    public static void main(String[] args) {
+        
+        //create an ArrayList
+        ArrayList<String> list = new ArrayList<String>();
+        //Adding elements to the ArrayList
+
+        
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Pineapple");
+        System.out.println(list);
+        
+        //Adding more elements to the ArrayList
+
+        list.add("Avocado");
+        list.add("Dragon fruit");
+        System.out.println(list);
+    }
+
+}
